ef02a2ddc788dd44e37e71dccee6f8a3501b3182
Courtland Erickson <courtland.erickson@gmail.com>
2025-12-05 19:06:38 -0600

    Updated completed garden management lab

--- gardenManagement.js
+++ gardenManagement.js
@@ -8,10 +8,10 @@ if (temperature > 80) {
     console.log("Watering off")
 }
 
-if (timeOfDay === "morning") {
-    console.log("Lights off")
-} else {
+if (timeOfDay === "evening" || "night") {
     console.log("Lights on")
+} else {
+    console.log("Lights of")
 }
 
 while (soilMoisture < 40) {
